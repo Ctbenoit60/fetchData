69ac658e00b61d8860c419567e8c58d539a4e945
Ctbenoit60 <ctbenoit60@gmail.com>
2023-11-27 20:36:31 -0600

    added the get fetch and iteration for json data

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-fetch('https://jsonplaceholder.typicode.com/posts', {
-method: 'POST', // GET is default, but we can also use POST
-body: JSON.stringify({ // POSTed data goes in the request body
-title: 'The Studio',
-body: 'Something funny',
-userId: 1,
-}),
-headers: {
-'Content-type': 'application/json; charset=UTF-8',
-},
-})
-.then((response) => response.json())
-.then((json) => console.log(json));
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+// method: 'POST', // GET is default, but we can also use POST
+// body: JSON.stringify({ // POSTed data goes in the request body
+// title: 'The Studio',
+// body: 'Something funny',
+// userId: 1,
+// }),
+// headers: {
+// 'Content-type': 'application/json; charset=UTF-8',
+// },
+// })
+// .then((response) => response.json())
+// .then((json) => console.log(json));
 
-function addCard(title ="Sample Title", userId = " Sample User", id = "Sample ID", body = "Sample Body") {
-    const template =
-    document.getElementById("card-template").content.cloneNode(true);
-    template.querySelector('.card-title').innerText = title;
-    template.querySelector('.card-userId').innerText = userId;
-    template.querySelector('.card-id').innerText = id;
-    template.querySelector('.card-text').innerText = body;
-    document.querySelector('#card-list').appendChild(template);
-    }
+fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+  .then((response) => response.json())
+  .then((json) => json.forEach((person) => {
+    addCard(person.title, person.body)
+  }));
 
-    // json.forEach((person) => {
-    //     addCard(person.title, person.body);
-    //   });
+function addCard(
+  title = "Sample Title",
+  userId = " Sample User",
+  id = "Sample ID",
+  body = "Sample Body"
+) {
+  const template = document
+    .getElementById("card-template")
+    .content.cloneNode(true);
+  template.querySelector(".card-title").innerText = title;
+  template.querySelector(".card-userId").innerText = userId;
+  template.querySelector(".card-id").innerText = id;
+  template.querySelector(".card-text").innerText = body;
+  document.querySelector("#card-list").appendChild(template);
+}
 
-
+// json.forEach((person) => {
+//     addCard(person.title, person.body);
+//   });
